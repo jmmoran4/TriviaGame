6610d55a882444aa0ae8c110f2e08fa254188205
jmmoran4 <jmmoran@buffalo.edu>
2024-04-01 17:50:07 -0700

    Switch to MongoDB

--- Backend_Entry.py
+++ Backend_Entry.py
@@ -1,17 +1,28 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, jsonify
+from DB import DatabaseOperations
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TriviaGame.db'
-db = SQLAlchemy(app)
-
-with app.app_context():
-    db.create_all()
+db_ops = DatabaseOperations()
 
 @app.route('/')
 def home():
+    
     return 'Hello, Trivia Game!'
+
+@app.route('/add')
+def add_question(Q, A):
+    db_ops.add_question(Q, A)
+    return 'Question added successfully!'
+
+@app.route('/question/<question_id>')
+def get_question(question_id):
+    question = db_ops.get_question_by_id(question_id)
+    return jsonify(question)
+
+@app.route('/random')
+def get_random_question():
+    question = db_ops.get_random_question()
+    return jsonify(question)
 
 if __name__ == '__main__':
     app.run(debug=True)
