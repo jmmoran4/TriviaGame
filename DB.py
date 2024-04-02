755938e6027c6baebc460ab32997423b18ea2d25
jmmoran4 <jmmoran@buffalo.edu>
2024-04-01 17:24:18 -0700

    Switched DB to mongoDB

--- DB.py
+++ DB.py
@@ -1,45 +1,37 @@
-from flask_sqlalchemy import SQLAlchemy
-from Backend_Entry import db
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import random
 
-
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(250), nullable=False)
-    anwser = db.Column(db.String(250), nullable=False)
-
+# MongoDB connection setup
+client = MongoClient('your_mongodb_connection_string')
+db = client.trivia_database
+Q_A_collection = db.questions
 
 class DatabaseOperations:
     def add_question(self, Q, A):
-        new_question = Question(question=Q, answer=A)
-        db.session.add(new_question)
-        db.session.commit()
+        """ Add new question(Q) with a matching answer(A) """
+        Q_A = {
+            "question": Q,
+            "answer": A
+        }
+        Q_A_collection.insert_one(Q_A)
 
-    def delete_question(self, question_id):
-        question = Question.query.get(question_id)
-        if question:
-            db.session.delete(question)
-            db.session.commit()
+    def delete_question(self, Q_A_ID):
+        """ Delete question and answer (Q_A) """
+        Q_A_collection.delete_one({"_id": ObjectId(Q_A_ID)})
 
-    def delete_question(self, question_id):
-        question = Question.query.get(question_id)
-        if question:
-            db.session.delete(question)
-            db.session.commit()
-
-    def get_question_by_id(self, question_id):
-        question = Question.query.get(question_id)
-        return question
+    def get_question_by_id(self, Q_A_ID):
+        """ Retrieve question and answer (Q_A_ID) """
+        return Q_A_collection.find_one({"_id": ObjectId(Q_A_ID)})
 
     def get_all_questions(self):
-        questions = Question.query.all()
-        return questions
+        """ Return a list of all questions and answers from DB """
+        return list(Q_A_collection.find())
 
+###
     def get_random_question(self):
-        import random
-        questions = Question.query.all()
-        if questions:
-            return random.choice(questions)
-        return None
+        """ Return a random question and answer from db or None if it's empty """
+        Q_As = list(Q_A_collection.find())
+        return random.choice(Q_As) if Q_As else None
 
 
